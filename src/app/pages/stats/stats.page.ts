86c60dd6f7eda50b0223bae88ae43ef9be32e330
flexocarpius <flexocarpius@gmail.com>
2021-04-16 13:18:56 -0700

    Added collapsing title and charts configuration.

--- src/app/pages/stats/stats.page.ts
+++ src/app/pages/stats/stats.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import * as Chart from 'chart.js';
 import { loadAllEntries } from '../../store/actions/entries.actions';
 import { AppState } from '../../store/app.state';
 
@@ -9,12 +10,12 @@ import { AppState } from '../../store/app.state';
   styleUrls: ['./stats.page.scss'],
 })
 export class StatsPage implements OnInit {
-  public buyData: any[];
-  public sellData: any[];
-  public lineChartLabels: any;
-  public lineChartOptions: (any & { annotation: any }) = {
+  buyChart: any;
+  sellChart: any;
+  lineChartOptions: (any & { annotation: any }) = {
     responsive: true,
-    maintainAspectRatio: true,
+    maintainAspectRatio: false,
+    legend: false,
     scales: {
       xAxes: [
         { 
@@ -74,21 +75,6 @@ export class StatsPage implements OnInit {
       }
     }
   };
-  public buyLineChartColors: any = [
-    {
-      borderColor: 'rgb(52, 235, 149)',
-      backgroundColor: 'rgba(255,0,0,0.3)',
-    },
-  ];
-  public sellLineChartColors: any = [
-    {
-      borderColor: 'rgb(235, 119, 52)',
-      backgroundColor: 'rgba(255,0,0,0.3)',
-    },
-  ];
-  public lineChartLegend = false;
-  public lineChartType = 'line';
-  public lineChartPlugins = [];
   loading: boolean;
 
   constructor(private store: Store<AppState>) { }
@@ -99,27 +85,64 @@ export class StatsPage implements OnInit {
       this.loading = loading;
       if (!loading && entries) {
         const reversed = entries.slice().reverse();
-        this.buyData = [
+        const buyData = [
           {
             data: reversed.map(d => d.buy_price),
             label: 'Buy price',
             pointRadius: 0,
             fill: false,
             borderWidth: 3,
+            borderColor: 'rgb(52, 235, 149)',
+            backgroundColor: 'rgba(255,0,0,0.3)',
           }
         ];
-        this.sellData = [
+        const sellData = [
           {
             data: reversed.map(d => d.sell_price),
             label: 'Sell price',
             pointRadius: 0,
             fill: false,
             borderWidth: 3,
+            borderColor: 'rgb(235, 119, 52)',
+            backgroundColor: 'rgba(255,0,0,0.3)',
           }
         ];
-        this.lineChartLabels = reversed.map(d => new Date(d.date))
+        const labels = reversed.map(d => new Date(d.date));
+        this.initBuyChart(labels, buyData);
+        this.initSellChart(labels, sellData);
       }
     });
   }
 
+  initBuyChart(labels, datasets) {
+    let ctx = document.getElementById("buy-chart") as HTMLCanvasElement;
+    this.buyChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels,
+        datasets,
+      },
+      options: this.lineChartOptions
+    });
+  }
+
+  initSellChart(labels, datasets) {
+    let ctx = document.getElementById("sell-chart") as HTMLCanvasElement;
+    this.sellChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels,
+        datasets,
+      },
+      options: this.lineChartOptions
+    });
+  }
+
+  resetBuyChartZoom() {
+    this.buyChart.resetZoom();
+  }
+
+  resetSellChartZoom() {
+    this.sellChart.resetZoom();
+  }
 }
